eb3de50e7020e9f4c3d173bf89f9d95f06931d2c
Lionel PLAIS <lionel.plais@ilp-web.net>
2020-12-09 01:27:01 +0100

    [Salvatore] [source:lang/ barreoutils] Export depuis https://trad.spip.net de la langue br [Salvatore] [source:lang/ barreoutils] Mise a jour du bilan depuis https://trad.spip.net

---
+++ lang/barreoutils_br.php
@@ -0,0 +1,59 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/barreoutils?lang_cible=br
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// B
+	'barre_a_accent_grave' => 'Enlakaat un À',
+	'barre_alignerdroite' => '[/Steudañ a-zehou/] ar rannbennad',
+	'barre_barre' => 'Barrennañ an destenn',
+	'barre_bulle' => 'Stlennglogorenn',
+	'barre_c_cedille_maj' => 'Enlakaat ur Ç',
+	'barre_caracteres' => 'Arouezioù ispisial',
+	'barre_centrer' => '[|Kreizhañ|] ar rannbennad',
+	'barre_chercher' => 'Klask Amsaviñ',
+	'barre_clean' => 'Naetañ ar c’hod eus holl balizennoù HTML',
+	'barre_code' => 'Lakaat e-stumm ur &lt;code&gt;c’hod stlenneg&lt;/code&gt;', # RELIRE
+	'barre_desindenter' => 'Digreskiñ ur rollenn',
+	'barre_e_accent_aigu' => 'Enlakaat un É',
+	'barre_e_accent_grave' => 'Enlakaat un È',
+	'barre_ea' => 'Enlakaat un æ',
+	'barre_ea_maj' => 'Enlakaat un Æ',
+	'barre_encadrer' => '[(Sternañ)] ar rannbennad',
+	'barre_eo' => 'Enlakaat un œ',
+	'barre_eo_maj' => 'Enlakaat un Œ',
+	'barre_euro' => 'Enlakaat ar simbol €',
+	'barre_exposant' => 'Lakaat an destenn e &lt;sup&gt;marc’her&lt;/sup&gt;',
+	'barre_galerie' => 'Digor ar skeudennaoueg',
+	'barre_gestion_caption' => 'Alc’houez ha Diverrañ',
+	'barre_gestion_cr_casse' => 'Doujañ da vent al lizherennoù',
+	'barre_gestion_cr_changercasse' => 'Chañch da vent al lizherennoù', # RELIRE
+	'barre_gestion_cr_changercassemajuscules' => 'Lakaat e pennlizherenn',
+	'barre_gestion_cr_changercasseminuscules' => 'Lakaat e lizherenn vunut',
+	'barre_gestion_cr_entier' => 'Ger a-bezh',
+	'barre_gestion_cr_remplacer' => 'Amsaviñ',
+	'barre_gestion_entete' => 'Talbenn',
+	'barre_gestion_taille' => 'Braster stabil',
+	'barre_guillemets' => 'Gronnañ gant « klochedigoù »',
+	'barre_guillemets_simples' => 'Gronnañ gant “klochedigoù eil live”',
+	'barre_indenter' => 'Kreskaat ur rollenn',
+	'barre_inserer_caracteres' => 'Enlakaat arouezioù ispisial',
+	'barre_lien_externe' => 'Liamm diavaez',
+	'barre_liste_ol' => 'Lakaat e rollenn niverennet',
+	'barre_liste_ul' => 'Lakaat e rollenn',
+	'barre_lorem_ipsum' => 'Enlakaat ur rannbennad faos',
+	'barre_lorem_ipsum_3' => 'Enlakaat teir rannbennad faos',
+	'barre_miseenevidence' => 'Lakaat an destenn [*anat*]',
+	'barre_paragraphe' => 'Krouiñ ur rannbennad',
+	'barre_petitescapitales' => 'Lakaat an destenn e &lt;sc&gt;pennlizherennoù bihan&lt;/sc&gt;',
+	'barre_tableau' => 'Lakaat/kemmañ (diuzañ anezhañ a-raok) un daolenn',
+
+	// E
+	'editer' => 'Embann'
+);
